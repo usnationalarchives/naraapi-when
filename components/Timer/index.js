dac7a56d2f1a9f8e955594a2f0626e2b0b3628d0
Daniel Gading <daniel.gading@agencychief.com>
2018-02-28 14:28:11 -0500

    Updates Timer with new animation and prop

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// The stroke-dasharray has to be equal to
+// the circumference of the svg circle
+
 class Timer extends React.Component {
 
   constructor(props) {
@@ -9,7 +12,9 @@ class Timer extends React.Component {
       timeLeft: props.gameTime,
       endGame: props.endGame,
       lowTime: false,
-      blink: false
+      size: props.size,
+      radius: (props.size / 2) - 5,
+      offset: props.size / 2
     }
     this.countDown = this.countDown.bind(this);
   }
@@ -31,8 +36,7 @@ class Timer extends React.Component {
 
     if(currentTime < 10) {
       this.setState(prevState => ({
-        lowTime: true,
-        blink: !prevState.blink
+        lowTime: true
       }));
     }
 
@@ -47,26 +51,87 @@ class Timer extends React.Component {
   }
 
   render() {
+    console.log(2 * Math.PI * this.state.radius)
     return (
       <div>
-        <p>
-          Time Left: 
-          <span>{this.state.timeLeft}</span>
-        </p>
-        
+        <span>{this.state.timeLeft}</span>
+        <svg>
+          <circle r={this.state.radius} cx={this.state.offset} cy={this.state.offset} style={{
+            stroke: 'rgba(255, 255, 255, .25)', 
+            animation: 'none'}}></circle>
+        </svg>
+        <svg>
+          <circle r={this.state.radius} cx={this.state.offset} cy={this.state.offset} 
+          style={{
+            stroke: 'rgba(17, 46, 81, 1)',
+            }}></circle>
+        </svg>
         <style jsx>{`
+            div {
+              position: relative;
+            }
             span {
-              color: ${ this.state.lowTime ? 'red' : 'black' };
-              visibility: ${ this.state.blink ? 'hidden' : 'visible'}
+              position: absolute;
+              text-align: center;
+              left: 0;
+              top: 0;
+            }
+            svg {
+              position: absolute;
+              left: 0;
+              top: 0;
+              transform: rotateY(180deg) rotateX(180deg) rotateZ(90deg);
+            }
+            svg circle {
+              stroke-dashoffset: 0px;
+              stroke-linecap: square;
+              stroke-width: 5px;
+              fill: none;
+              animation-timing-function: linear;
+              animation-delay: initial;
+              animation-iteration-countDown: infinite;
+              animation-direction: initial;
+              animation-fill-mode: forwards;
+              animation-play-state: initial;
             }
           `}</style>
+          <style jsx>{`
+            div {
+              width: ${this.state.size + 'px'};
+              height: ${this.state.size + 'px'};
+            }
+            svg {
+              width: ${this.state.size + 'px'};
+              height: ${this.state.size + 'px'};
+            }
+            span {
+              width: ${this.state.size + 'px'};
+              height: ${this.state.size + 'px'};
+              line-height: ${this.state.size + 'px'};
+              color: ${ this.state.lowTime ? '#fff' : '#fff' };
+            }
+            svg circle {
+            stroke-dasharray: ${Math.floor((2 * Math.PI * this.state.radius)) + 'px'};
+            animation-name: countdown;
+            animation-duration: ${this.props.gameTime + 's'};
+            }
+            @keyframes countdown {
+              from {
+                stroke-dashoffset: 0px;
+              }
+              to {
+                stroke-dashoffset: ${Math.floor((2 * Math.PI * this.state.radius)) + 'px'};
+              }
+            }
+            `}</style>
       </div>
     );
   }
 }
 
 Timer.propTypes = {
-  gameTime: PropTypes.number.isRequired
+  gameTime: PropTypes.number.isRequired,
+  size: PropTypes.number.isRequired
 };
 
 export default Timer;
