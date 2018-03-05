78288e9fa9ef3bdce88de1aa79bda7ace215f496
Charles Hwang <charles.hwang@agencychief.com>
2018-03-04 20:37:24 -0500

    NAPI-75: Create results page component

---
+++ components/Result/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../form/Button';
+import Score from '../Score';
+
+const Result = ({score, scoreType}) => {
+
+  return (
+    <div className={"result-container"}>
+      <p className={"result-header"}>Final Score</p>
+      <Score
+        score={score}
+        scoreType={'finalScore'}
+      />
+      <p><em>Great Job!</em></p>
+      <p className={"cta"}>Push yourself even further as a citizen activist.</p>
+      <Button
+        btnText={"Learn More"}
+        onClick={() => window.location = "https://catalog.archives.gov/"}
+        btnRole={'afterTextWide'}
+        btnColor={'blue'}
+      />
+      <div className={"game-options"}>
+        <Button
+          btnText={"Share Score"}
+          onClick={() => alert("Coming Soon!")}
+          btnRole={'afterTextNarrow'}
+          btnColor={'white'}
+        />
+        <Button
+          btnText={"Replay"}
+          onClick={() => location.reload()}
+          btnRole={'afterTextNarrow'}
+          btnColor={'white'}
+        />
+      </div>
+      <style jsx>{`
+        .result-container {
+          margin:12.5rem 0 0;
+        }
+        .result-container > div {
+          float: none;
+        }
+        .result-header {
+          font-size:1.6rem;
+          text-transform: uppercase;
+          letter-spacing: 1px;
+          line-height:1.2;
+          font-weight:600;
+          color:#132f50;
+        }
+        em {
+          font-size:2.3rem;
+          font-weight:600;
+          color:#132f50;
+          font-style:normal;
+        }
+        .cta {
+          font-size:1.75rem;
+          color:#132f50;
+        }
+        .game-options {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 10rem;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+
+export default Result;
